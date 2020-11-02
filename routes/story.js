849679f63fa04ffa7bbf0720a8fc16e1fcca7343
AUTISM81 <christian.backstrom@elev.ga.ntig.se>
2020-11-02 10:28:44 +0100

    removed /story so only id is avalible since index is the same

--- routes/story.js
+++ routes/story.js
@@ -2,26 +2,6 @@ var express = require('express');
 var router = express.Router();
 const {pool, query} = require('../models/db');
 
-router.get('/', async function (req, res, next) {
-  try {
-    const story = await query(
-      'SELECT * FROM story where id = 1',
-    );
-
-    const links = await query (
-      'SELECT * FROM links where story_id = 1'
-    );
-
-    res.render('story', {
-      story: story,
-      links: links
-    });
-  } catch (e) {
-    console.error(e);
-    next(e);
-  };
-})
-
 router.get('/:id', async function (req, res, next) {
   try {
     const story = await query(
